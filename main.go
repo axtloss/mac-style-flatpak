bc7278b80933f01190b76e970276e24a6d1cf353
axtlos <axtlos@tar.black>
2022-04-08 16:43:57 +0200

    add support for uninstalling applications

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -22,25 +23,59 @@ func symlink(filepath string, path string) {
 	fmt.Println(string(stdout))
 }
 
+func uninstall(filepath string, path string) {
+	cmd := exec.Command("flatpak", "remove", "-y", strings.Split(strings.Replace(filepath, ".desktop", "", 1), "/")[1])
+	stdout, err := cmd.Output()
+	if err != nil {
+		beeeper := beeep.Notify("Error", "Unable to uninstall application "+strings.Split(strings.Replace(filepath, ".desktop", "", 1), "/")[1], "assets/notif.png")
+		if beeeper != nil {
+			panic(beeeper)
+		}
+	} else {
+		beeper := beeep.Notify("Mac Style Uninstall", strings.Split(strings.Replace(filepath, ".desktop", "", 1), "/")[1]+" uninstalled succesfully", "assets/notif.png")
+		if beeper != nil {
+			panic(beeper)
+		}
+	}
+	fmt.Println(string(stdout))
+}
+
 func install(filepath string, path string) {
 	cmd := exec.Command("flatpak", "install", "--user", "-y", filepath)
 	stdout, err := cmd.Output()
 	if err != nil {
-		log.Fatal(err)
-	} else {
-		if err := os.Remove("./" + filepath); err != nil {
-
-			beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" failed to install!", "assets/notif.png")
-			if beeperr != nil {
-				panic(beeperr)
-			}
-			log.Fatal(err)
-		}
-		symlink(filepath, path)
-		beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" installed succesfully", "assets/notif.png")
+		beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" failed to install!", "assets/notif.png")
 		if beeperr != nil {
 			panic(beeperr)
 		}
+	} else {
+		symlink(filepath, path)
+
+		symlink(filepath, path)
+
+		if _, err := os.Stat(filepath); err == nil {
+			if err := os.Remove(filepath); err != nil {
+				log.Fatal(err)
+			}
+			beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" installed succesfully", "assets/notif.png")
+			if beeperr != nil {
+				panic(beeperr)
+			}
+		} else if errors.Is(err, os.ErrNotExist) {
+			beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" installed succesfully", "assets/notif.png")
+			if beeperr != nil {
+				panic(beeperr)
+			}
+		} else {
+			if err := os.Remove(filepath); err != nil {
+				log.Fatal(err)
+			}
+			beeperr := beeep.Notify("Mac Style Install", strings.Split(strings.Replace(filepath, ".flatpakref", "", 1), "/")[1]+" installed succesfully", "assets/notif.png")
+			if beeperr != nil {
+				panic(beeperr)
+			}
+		}
+
 	}
 	fmt.Println(string(stdout))
 
@@ -67,6 +102,10 @@ func main() {
 					fmt.Println("event:", event)
 					fmt.Println("filepath:", event.Name)
 					install(event.Name, path)
+				} else if strings.Contains(event.String(), "RENAME") && strings.Contains(event.String(), ".desktop") {
+					fmt.Println("event:", event)
+					fmt.Println("filepath:", event.Name)
+					uninstall(event.Name, path)
 				}
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					fmt.Println("modified file:", event.Name)
